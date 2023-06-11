52421ce2d2bde2c382ad5640d494453a4fbd416b
Hannah Lappe <hannah@lappe.page>
2023-06-11 10:02:01 +0200

    added error handling

--- frontend/src/lib/server/services/MeasurementService.ts
+++ frontend/src/lib/server/services/MeasurementService.ts
@@ -2,35 +2,37 @@ import SolarApi from '../api/solarmax';
 import { db } from '../db/db';
 
 export async function createMeasurement() {
-		const measurement = await SolarApi.getMeasurement(fetch);
+	const measurement = await SolarApi.getMeasurement(fetch);
 
-		const ids = await db
-			.selectFrom('inverter')
-			.select(['id', 'addr'])
-			.where(
-				'addr',
-				'in',
-				measurement.map((m) => m.addr)
-			)
-			.execute();
+	if (measurement.length === 0) throw new Error('No measurement data');
 
-		await db
-			.insertInto('measurement')
-			.values(
-				measurement.map((m) => {
-					const inverter_id = ids.find((i) => i.addr === m.addr)?.id;
+	const ids = await db
+		.selectFrom('inverter')
+		.select(['id', 'addr'])
+		.where(
+			'addr',
+			'in',
+			measurement.map((m) => m.addr)
+		)
+		.execute();
 
-					if (!inverter_id) throw new Error('Inverter not found');
+	await db
+		.insertInto('measurement')
+		.values(
+			measurement.map((m) => {
+				const inverter_id = ids.find((i) => i.addr === m.addr)?.id;
 
-					return {
-						inverter_id,
-						fdat: new Date(m.fdat).toISOString(),
-						pac: m.pac,
-						pdc: m.pdc,
-						kdy: m.kdy,
-						kt0: m.kt0
-					};
-				})
-			)
-			.execute();
+				if (!inverter_id) throw new Error('Inverter not found');
+
+				return {
+					inverter_id,
+					fdat: new Date(m.fdat).toISOString(),
+					pac: m.pac,
+					pdc: m.pdc,
+					kdy: m.kdy,
+					kt0: m.kt0
+				};
+			})
+		)
+		.execute();
 }
